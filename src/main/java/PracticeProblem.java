8a515c96b55f212eaac79db0f604653cbfad0bbc
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-02-12 16:23:01 +0000

    commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,9 @@
+/*
+File Name: Lesson 1.3 - Strings
+Author: Stephen Li
+Date Created: Feb 12, 2026
+Date Last Modified: Feb 12, 2026
+*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +15,31 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		String Gaycat;
+		Gaycat = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+		System.out.println(Gaycat);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String word1 = "Fortnite ";
+		String word2 = "is awful";
+		System.out.print(word1);
+		System.out.println(word2);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		String word1 = "Don't ";
+		String word2 = "@ me";
+		System.out.println(word1 + word2);
 	}
 
 
